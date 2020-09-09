97c21629a8c2eeecaaceafbab06d1e7e90c11852
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-09-09 23:34:24 +0200

    Updates

---
+++ precache-manifest.2e2a89220061147951c5170ddc2b9578.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a11cfdd9ad6701bbbee5f99fa2ac508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a72274e63851ec9a27e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0380f2336218ceb526af",
+    "url": "/static/js/2.abddc77d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.abddc77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a72274e63851ec9a27e",
+    "url": "/static/js/main.bb1a2760.chunk.js"
+  },
+  {
+    "revision": "dee31d2b8611699e46ca",
+    "url": "/static/js/runtime-main.9b6e31e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
