7ab69349827179b614da9989832858d5f319c82b
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-09-16 10:38:45 +0200

    add home

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Page from "../Page";
 
+import "../../css/Home.scss";
+
 export class Home extends Component {
   render() {
     return (
       <Page>
         <div className="Home" id="Home">
-          <h1>Home</h1>
+          <div className="homeContent">
+            <h1>{/*Hello There!*/}</h1>
+          </div>
         </div>
       </Page>
     );
