e1361774a2db675647aee88af4b428c025a1ec42
Adonis Stavridis <adonis.stavridis@gmail.com>
2021-10-10 02:33:55 +0200

    scrollbar styling

--- src/components/Pages/Contact.tsx
+++ src/components/Pages/Contact.tsx
@@ -19,83 +19,115 @@ export class Contact extends Component {
 					<table className="contactInfo">
 						<tbody>
 							<tr>
-								<a
-									href="https://goo.gl/maps/92ezbLcYhQcgeNig9"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<td>
+								<td>
+									<a
+										href="https://goo.gl/maps/92ezbLcYhQcgeNig9"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
 										<img
 											className="contactLogo"
 											src={HomeLogo}
 											alt="Home-logo"
 										/>
-									</td>
-									<td>
+									</a>
+								</td>
+								<td>
+									<a
+										href="https://goo.gl/maps/92ezbLcYhQcgeNig9"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
 										2 rue de la Colonne, 67100
 										<br /> Strasbourg, France
-									</td>
-								</a>
+									</a>
+								</td>
 							</tr>
 							<tr>
-								<a
-									href="mailto:adonis.stavridis@gmail.com"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<td>
+								<td>
+									<a
+										href="mailto:adonis.stavridis@gmail.com"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
 										<img
 											className="contactLogo"
 											src={GMailLogo}
 											alt="GMail-logo"
 										/>
-									</td>
-									<td>adonis.stavridis@gmail.com</td>
-								</a>
+									</a>
+								</td>
+								<td>
+									<a
+										href="mailto:adonis.stavridis@gmail.com"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										adonis.stavridis@gmail.com
+									</a>
+								</td>
 							</tr>
 							<tr>
-								<a href="tel:+33782505580">
-									<td>
+								<td>
+									<a href="tel:+33782505580">
 										<img
 											className="contactLogo"
 											src={PhoneLogo}
 											alt="Phone-logo"
 										/>
-									</td>
-									<td>(+33) 7 82 50 55 80</td>
-								</a>
+									</a>
+								</td>
+								<td>
+									<a href="tel:+33782505580">(+33) 7 82 50 55 80</a>
+								</td>
 							</tr>
 							<tr>
-								<a
-									href="https://github.com/Adonis-Stavridis"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<td>
+								<td>
+									<a
+										href="https://github.com/Adonis-Stavridis"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
 										<img
 											className="contactLogo"
 											src={GitHubLogo}
 											alt="GitHub-logo"
 										/>
-									</td>
-									<td>Adonis-Stavridis</td>
-								</a>
+									</a>
+								</td>
+								<td>
+									<a
+										href="https://github.com/Adonis-Stavridis"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Adonis-Stavridis
+									</a>
+								</td>
 							</tr>
 							<tr>
-								<a
-									href="https://www.linkedin.com/in/adonis-stavridis-profile/"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<td>
+								<td>
+									<a
+										href="https://www.linkedin.com/in/adonis-stavridis-profile/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
 										<img
 											className="contactLogo"
 											src={LinkedInLogo}
 											alt="LinkedIn-logo"
 										/>
-									</td>
-									<td>Adonis Stavridis</td>
-								</a>
+									</a>
+								</td>
+								<td>
+									<a
+										href="https://www.linkedin.com/in/adonis-stavridis-profile/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Adonis Stavridis
+									</a>
+								</td>
 							</tr>
 						</tbody>
 					</table>
